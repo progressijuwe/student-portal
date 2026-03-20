c3c21348a2a6141ac4eaff26c1fa148cfd736153
progressijuwe <yenumijuwe@gmail.com>
2026-03-20 18:00:11 +0100

    Login Page

--- src/router.jsx
+++ src/router.jsx
@@ -1,15 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
+import LoginPage from './pages/LoginPage'
 
 export const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
   {
     path: '/',
     element: <MainLayout />,
     children: [
       { index: true,    element: <HomePage /> },
-      { path: 'about',  element: <AboutPage /> },
     ],
   },
 ])
