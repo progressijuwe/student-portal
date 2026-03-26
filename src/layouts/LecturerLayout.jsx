5bc79de0ea114d937ed7ccc95f35eaa6a9ea0ced
progressijuwe <yenumijuwe@gmail.com>
2026-03-26 15:19:51 +0100

    fix: layout scroll issue

--- src/layouts/LecturerLayout.jsx
+++ src/layouts/LecturerLayout.jsx
@@ -4,9 +4,9 @@ import Sidebar  from '../sections/shared/Sidebar/Sidebar'
 
 export default function LecturerLayout() {
   return (
-    <div className="flex h-screen overflow-hidden w-full">
+    <div className="flex md:h-screen md:overflow-hidden w-full">
       <Sidebar role="Lecturer" />
-      <div className='flex flex-col w-full overflow-y-auto'>
+      <div className='flex flex-col w-full md:overflow-y-auto'>
         <Header role="Lecturer"/>
         <main className="flex-1 font-body bg-[#F9F9FFFC]">
           <Outlet />
