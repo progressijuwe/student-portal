31f9c307cd134982bdc0b2397f592c043fd67489
progressijuwe <yenumijuwe@gmail.com>
2026-04-01 19:13:21 +0100

    feature: Course Pane Section

--- src/pages/lecturer/LecturerCoursesPage.jsx
+++ src/pages/lecturer/LecturerCoursesPage.jsx
@@ -1,11 +1,13 @@
+import CoursePane from "../../sections/lecturer/courses/CoursePane";
 import CourseSelect from "../../sections/lecturer/courses/CourseSelect";
 
 
 export default function LecturerCoursesPage(){
 
     return(
-        <div className="px-5 py-8">
+        <div className="px-5 py-8 flex flex-col gap-6">
             <CourseSelect />
+            <CoursePane />
         </div>
     )
 }
--- src/sections/lecturer/courses/CourseSelect.jsx
+++ src/sections/lecturer/courses/CourseSelect.jsx
@@ -10,7 +10,7 @@ export default function CourseSelect(){
 
     return(
         <section className="flex flex-col gap-2 w-full max-w-185">
-            <h2 className="text-base text-label font-medium">Select Course</h2>
+            <h2 className="text-sm md:text-base text-label font-medium">Select Course</h2>
             <select defaultValue="" className="border border-[#D1D5DC] rounded-[10px] p-2">
                 <option value="" disabled></option>
                 {selectOptions?.filter(opt => opt.value !== '').map((opt) => (
